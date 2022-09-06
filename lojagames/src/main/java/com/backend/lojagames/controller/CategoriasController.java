94261aff706863829b6a79de32082d429ea02ae0
unknown <wes_lima@outlook.com>
2022-09-06 14:47:04 -0300

    Adicionando o CRUD até o Post

--- lojagames/src/main/java/com/backend/lojagames/controller/CategoriasController.java
+++ lojagames/src/main/java/com/backend/lojagames/controller/CategoriasController.java
@@ -2,10 +2,16 @@ package com.backend.lojagames.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +29,23 @@ public class CategoriasController {
 	@GetMapping
 	public ResponseEntity<List<Categorias>> getAll(){
 		return ResponseEntity.ok(categoriasRepository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Categorias> getById(@PathVariable Long id){
+		return categoriasRepository.findById(id)
+				.map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	
+	@GetMapping("/ps4/{ps4}")
+	public ResponseEntity<List<Categorias>> getByPs4(@PathVariable String ps4) {
+		return ResponseEntity.ok(categoriasRepository.findAllByPs4ContainingIgnoreCase(ps4));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Categorias> post(@Valid @RequestBody Categorias categorias){
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(categoriasRepository.save(categorias));
 	}
 }
--- lojagames/src/main/java/com/backend/lojagames/repository/CategoriasRepository.java
+++ lojagames/src/main/java/com/backend/lojagames/repository/CategoriasRepository.java
@@ -1,11 +1,15 @@
 package com.backend.lojagames.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.backend.lojagames.model.Categorias;
 
 @Repository
 public interface CategoriasRepository extends JpaRepository<Categorias, Long> {
-
+	
+	public List <Categorias> findAllByPs4ContainingIgnoreCase(@Param("ps4") String ps4);
 }
